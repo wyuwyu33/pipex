dff3e043a4e05229e33d444f12dc92280c9b063d
유원우 <wyu@yuwon-uui-MacBookAir.local>
2022-06-27 20:26:38 +0900

    fix malloc error

--- exec.c
+++ exec.c
@@ -6,30 +6,68 @@
 /*   By: wyu <wyu@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 16:45:21 by wyu               #+#    #+#             */
-/*   Updated: 2022/06/27 17:17:43 by wyu              ###   ########.fr       */
+/*   Updated: 2022/06/27 20:21:31 by wyu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+char	*strpdup(char *s1, char *s2)
+{
+	char	*res;
+	int		i;
+
+	if (!s2)
+		s2 = s1 + ft_strlen(s1);
+	res = (char *)malloc(sizeof(char) * (s2 - s1 + 1));
+	if (!res)
+		return (NULL);
+	i = -1;
+	while (s1 + (++i) < s2)
+		res[i] = *(s1 + i);
+	res[i] = '\0';
+	return (res);
+}
+
+char	*str_union(char *s1, char *s2)
+{
+	char	*res;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	res = (char *)malloc(sizeof(char) * (s1_len + s2_len + 2));
+	if (!res)
+		return (NULL);
+	ft_strlcat(res, s1, ft_strlen(s1) + 1);
+	ft_strlcat(res + s1_len, "/", 2);
+	ft_strlcat(res + s1_len + 1, s2, s2_len + 1);
+	return (res);
+}
+
 char	*parse_path(char *path, char *cmd)
 {
-	char	*result;
-	char	*tmp;
+	char	*colon;
+	char	*res;
 
-	result = ft_strchdup(path, ':');
-	tmp = ft_strjoin(result, "/");
-	free(result);
-	result = ft_strjoin(tmp, cmd);
-	free(tmp);
-	return (result);
+	colon = ft_strchr(path, ':');
+	if (colon)
+		path = strpdup(path, colon);
+	else
+		path = strpdup(path, NULL);
+	if (!path)
+		return (NULL);
+	res = str_union(path, cmd);
+	free(path);
+	return (res);
 }
 
 char	*get_path(char *cmd, char **envp)
 {
 	int		i;
 	char	*path;
-	char	*result;
+	char	*res;
 
 	i = -1;
 	while (envp[++i] && ft_strncmp(envp[i], "PATH=", ft_strlen("PATH=")))
@@ -39,10 +77,10 @@ char	*get_path(char *cmd, char **envp)
 	path = envp[i] + ft_strlen("PATH=");
 	while (1)
 	{
-		result = parse_path(path, cmd);
-		if (!access(result, F_OK | X_OK))
-			return (result);
-		free(result);
+		res = parse_path(path, cmd);
+		if (!access(res, F_OK | X_OK))
+			return (res);
+		free(res);
 		path = ft_strchr(path, ':');
 		if (!path)
 			break ;
